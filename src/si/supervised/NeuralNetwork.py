ae2ed4334a97949b2b2e5977ad9b720bfede3455
pg42876 <pg42876@alunos.uminho.pt>
2022-01-30 01:52:38 +0000

    Avaliação 31Jan V4

--- src/si/supervised/NeuralNetwork.py
+++ src/si/supervised/NeuralNetwork.py
@@ -90,6 +90,10 @@ class NN(Model):
         self.loss = mse
         self.loss_prime = mse_prime
 
+    def useLoss(self, loss, loss_prime):
+        self.loss = loss
+        self.loss_prime = loss_prime
+    
     def add(self, layer):
         self.layers.append(layer)
     
@@ -238,12 +242,11 @@ class Pooling2D(Layer):
         n, h, w, d = input.shape  # comprimento, altura e número das imagens
         h_out = (h - self.size) / self.stride + 1
         w_out = (w - self.size) / self.stride + 1
-        if not w_out.is_intiger() or not h_out.is_intiger():
+        if not w_out.is_integer() or not h_out.is_integer():
             raise Exception("Invalid output dimension")
         h_out, w_out = int(h_out), int(w_out)
-        X_reshaped = input.reshape()
-        self.X_col = im2col(X_reshaped, self.size, pad = 0, stride = self.stride)
-
+        X_reshaped = input.reshape(n * d, h, w, 1)
+        self.X_col, _ = im2col(X_reshaped, (self.size, self.size, d, d), pad = 0, stride = self.stride)
         out, self.max_idx = self.pool(self.X_col)
         out = out.reshape(h_out, w_out, n, d)
         out = out.transpose(3, 2, 0, 1)
@@ -258,49 +261,16 @@ class Pooling2D(Layer):
         dX = dX.reshape(self.X_shape)
         return dX
 
-class MaxPooling(Pooling2D):
+class MaxPooling2D(Pooling2D):
 
-    def __init__(self, region_shape, size = 2, stride = 2):
-        self.region_h, self.region_w = region_shape
-        self.size = size
-        self.stride = stride
-
-    def pool(X_col):
-        max_idx = np.argmax(X_col, axis=0)
+    def pool(self,X_col):
+        max_idx = np.argmax(X_col, axis = 0)
         out = X_col[max_idx, range(max_idx.size)]
         return out, max_idx
 
-    def dpool(dX_col, dout_col, pool_cache):
+    def dpool(self, dX_col, dout_col, pool_cache):
         dX_col[pool_cache, range(dout_col.size)] = dout_col
         return dX_col
 
-    def forward(self, input_data):
-        self.X_input = input_data
-        _, self.input_h, self.input_w, self.input_f = input_data.shape
-
-        self.out_h = self.input_h // self.region_h
-        self.out_w = self.input_w // self.region_w
-        output = np.zeros((self.out_h, self.out_w, self.input_f))
-
-        for image, i, j in self.iterate_regions():
-            output[i, j] = np.amax(image)
-        return output
-
-    def backward(self, output_error, lr):
-        n, w, h, d = self.X_shape
-
-        dX_col = np.zeros_like(self.X_shape)
-        dout_col = output_error.transpose(2, 3, 0, 1).ravel()
-
-        dX = self.dpool(dX_col, dout_col, self.max_idx)
-
-        dX = col2im(dX_col, (n * d, 1, h, w), self.size, padding = 0, stride = self.stride)
-        dX = dX.reshape(self.X_shape)
-
-        return dX
-
-    def iterate_regions(self):
-        for i in range(self.out_h):
-            for j in range(self.out_w):
-                image = self.X_input[(i * self.region_h) : (i * self.region_h + 2), (j * self.region_h) : (j * self.region_h + 2)]
-                yield image, i, j
+    def __str__(self):
+        return "MaxPooling2D"
